4b8e13b1b6059464b6b7884db1d741f4ddef65c5
Evantopian <46168209+ThisGuyEvan@users.noreply.github.com>
2021-11-05 03:44:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,27 +12,15 @@ Assignment: Lab 08
 #include "funcs.h"
 
 int main(){
-  std::string input_file = "inImage.pgm";
+  std::string imgs = "inImage.pgm";
   int image[MAX_H][MAX_W];
-  int height, width;
-  //Task A
-  invert(input_file, image);
 
-  //Task B
-  rightInvert(input_file, image);
-
-  //Task C
-  whiteBox(input_file, image);
-
-
-  //Task D
-  addFrame(input_file, image);
-
-  //Task E
-  scale(input_file, image);
-
-  //Task F
-  pixelate(input_file, image);
+  invert(imgs, image);
+  rightInvert(imgs, image);
+  whiteBox(imgs, image);
+  addFrame(imgs, image);
+  scale(imgs, image);
+  pixelate(imgs, image);
 
   return 0;
 }
